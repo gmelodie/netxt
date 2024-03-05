25d647afb706d8ad84ba0334d9eea5a1fe9d8046
Gabriel Cruz <gabrieldemelocruz@gmail.com>
2024-03-05 07:52:27 -0300

    Add cli skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use docopt::*;
-static USAGE: &'static str = "
-Usage: net [options] <todo-file>
-       net --help
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 
-Options:
-    -h, --help     Show this usage message.
-";
+// net init (create new)
+// net todo.txt (parse)
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Existing TODO file to parse
+    #[arg(short, long, value_name = "FILE")]
+    todo_file: Option<PathBuf>,
+
+    #[arg(short, long)]
+    init: Option<bool>,
+}
+
 fn main() {
+    let cli = Cli::parse();
     println!("Never ending TXT");
 }
